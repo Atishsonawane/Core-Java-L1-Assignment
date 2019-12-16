8595551badd394784e48eda19f4066afb4f2261a
Atishsonawane <atish13nov@gmail.com>
2019-12-16 12:42:52 +0530

    Update Assignment_1.java

--- Topic 1 : JVM Concepts and Language Basics/Assignment_1.java
+++ Topic 1 : JVM Concepts and Language Basics/Assignment_1.java
@@ -1,3 +1,9 @@
+/*
+
+AIM : Write a java program to display “Welcome to Java Programming” and then print your name on a separate line.
+
+*/
+
 public class Main
 {
 	public static void main(String[] args) {
