275b48dfc0fa0d5f14ab715ba26b75dd31869bcb
Atishsonawane <atish13nov@gmail.com>
2020-01-02 12:04:04 +0530

    Update Assignment_1.java

--- Topic 1 : JVM Concepts and Language Basics/Assignment_1.java
+++ Topic 1 : JVM Concepts and Language Basics/Assignment_1.java
@@ -8,7 +8,7 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Welcom to Java Programming.");
+		System.out.println("Welcome to Java Programming.");
 		System.out.println("My name is Atish Sonawane.");
 	}
 }
