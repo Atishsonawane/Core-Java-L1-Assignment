b55b8b509cfa81c8c474cb2aeb4c47817b0e314c
Atishsonawane <atish13nov@gmail.com>
2020-01-03 16:12:48 +0530

    Create CreditCardPayment.java

---
+++ Topic 2 : Object Oriented Concepts/Assignment_4/CreditCardPayment.java
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Define a class named Payment that contains a member variable of type double that stores the
+amount of the payment and appropriate accessor and mutator methods. Also create a method
+named paymentDetails that outputs an English sentence to describe the amount of the payment.
+
+Next, define a class named CashPayment that is derived from Payment. This class should
+redefine the paymentDetails method to indicate that the payment is in cash. Include appropriate
+constructor(s).
+
+Define a class named CreditCardPayment that is derived from Payment. This class should
+contain member variables for the name on the card, expiration date, and credit card number.
+Include appropriate constructor(s). Finally, redefine the paymentDetails method to include all
+credit card information in the printout.
+Create a main method that creates at least two CashPayment and two
+CreditCardPayment objects with different values and calls paymentDetails for each.
+
+*******************************************************************************/
+
+public class CreditCardPayment extends Payment 
+{
+
+	public String name, expDate, number;
+
+	CreditCardPayment(double value, String name, String expDate, String number) 
+	{
+		super(value);
+		this.number = number;
+		this.expDate = expDate;
+		this.name = name;
+	}
+
+	public void paymentDetails() {
+	System.out.println("The payment of Rs." + this.cash + " through the card " + this.number
+        + ", and expire date "	+ this.expDate + ", and the owner name: " + this.name + ".");
+	}
+
+}
